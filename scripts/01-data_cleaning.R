6117acffb5582825ddfd0be075b6bac1ec6ad55b
FanYang111 <fanmartin.yang@mail.utoronto.ca>
2022-02-06 21:14:22 -0500

    3

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,14 +1,10 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the survey data downloaded from open data Toronto
+# Author: Fan Yang
+# Data: 3rd February 2021
+# Contact: fanmartin.yang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
+
 
 
 
